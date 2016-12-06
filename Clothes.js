f79d9a96cf6582a95d6bbeba2558b34dcc7d3f6f
Claire <claire@Amandas-MacBook-Air.local>
2016-12-06 12:35:38 -0800

    proportional clothes

--- Clothes.js
+++ Clothes.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 function triangleshirt(){
 	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(300,150,100,100);
+	ctx.fillRect(300,150,100,150);
 
   	var path =new Path2D();
     path.moveTo(300,120);
@@ -41,15 +41,15 @@ shortsleeve()
 function pants1(){
 	ctx.fillStyle = "#FFFF00";
 	var path = new Path2D;
-	path.moveTo(300,250);
+	path.moveTo(300,300);
 
-	path.lineTo(275,350);
-	path.lineTo(310,350);
-	path.lineTo(350,250);
+	path.lineTo(275,450);
+	path.lineTo(310,450);
+	path.lineTo(350,300);
 
-	path.lineTo(390,350);
-	path.lineTo(425,350);
-	path.lineTo(400,250);
+	path.lineTo(390,450);
+	path.lineTo(425,450);
+	path.lineTo(400,300);
 	ctx.fill(path);
 }
 
@@ -57,9 +57,25 @@ pants1()
 
 function shoes1(){
 	ctx.fillStyle = "#0000FF";
-	ctx.fillRect(390,350,60,25);
-	ctx.fillRect(310,350,-60,25);
+	ctx.fillRect(390,450,60,25);
+	ctx.fillRect(310,450,-60,25);
 }
 
 shoes1()
+
+function size(){
+	ctx.fillStyle = "#FF5500";
+	var path = new Path2D;
+	path.moveTo(200,125);
+	//shoulders top
+
+	path.lineTo(300,50);
+	//head top
+	path.lineTo(300,300);
+	//hip area, where pants begin
+	path.lineTo(200,450);
+	//top of shoe
+
+	ctx.fill(path);
+}
 
