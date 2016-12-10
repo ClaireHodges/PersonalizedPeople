96a99d695778135b489eb774358b8237c41aa42a
Claire <claire@Amandas-Air.lan>
2016-12-09 17:56:36 -0800

    new clothes added

--- Clothes.js
+++ Clothes.js
@@ -2,7 +2,8 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 function triangleshirt(){
-	ctx.fillStyle = "#FF0000";
+	ctx.fillStyle = "#D3D3D3";
+	//default color, apply later!
 	ctx.fillRect(300,150,100,150);
 
   	var path =new Path2D();
@@ -18,7 +19,39 @@ function triangleshirt(){
 	ctx.fill(path);
 }
 
-triangleshirt()
+
+
+function curvedshirt(){
+	ctx.fillStyle = "#D3D3D3";
+	//default color, apply later!
+	ctx.fillRect(300,150,100,150);
+
+	var path=new Path2D();
+	path.arc(350,90,50,0.2*Math.PI,0.8*Math.PI);
+	path.lineWidth = 1;
+	path.lineTo(300,120);
+	path.lineTo(350,150);
+	path.lineTo(400,120);
+	path.lineTo(390,120);
+	ctx.fill(path);
+
+
+
+  	var path =new Path2D();
+    path.moveTo(300,120);
+    path.lineTo(350,150);
+    path.lineTo(300,150);
+    ctx.fill(path);
+
+    var path = new Path2D;
+	path.moveTo(400,120);
+	path.lineTo(350,150);
+	path.lineTo(400,150);
+	ctx.fill(path);
+
+}
+
+curvedshirt()
 
 function shortsleeve(){
 	var path = new Path2D;
@@ -32,11 +65,27 @@ function shortsleeve(){
 	path.moveTo(400,120);
 	path.lineTo(350,150);
 	path.lineTo(425,200);
-	path.lineTo(460,160)
+	path.lineTo(460,160);
 	ctx.fill(path);
 }
 
-shortsleeve()
+function longsleeve(){
+	var path = new Path2D;
+	path.moveTo(300,120);
+	path.lineTo(350,150);
+	path.lineTo(175,250); //ll
+	path.lineTo(150,220); //tl
+	ctx.fill(path);
+
+	var path = new Path2D;
+	path.moveTo(400,120);
+	path.lineTo(350,150);
+	path.lineTo(525,250); //lr
+	path.lineTo(550,220); //tr
+	ctx.fill(path);
+}
+
+longsleeve()
 
 function pants1(){
 	ctx.fillStyle = "#FFFF00";
@@ -55,6 +104,25 @@ function pants1(){
 
 pants1()
 
+function shorts1(){
+	ctx.fillStyle = "#FF0000";
+	var path = new Path2D;
+	path.moveTo(300,300);
+
+	path.lineTo(275,450);
+	path.lineTo(310,450);
+	path.lineTo(350,300);
+
+	path.lineTo(390,450);
+	path.lineTo(400,425);
+	path.lineTo(400,300);
+	ctx.fill(path);
+}
+
+shorts1()
+
+
+
 function shoes1(){
 	ctx.fillStyle = "#0000FF";
 	ctx.fillRect(390,450,60,25);
@@ -63,7 +131,9 @@ function shoes1(){
 
 shoes1()
 
-function size(){
+
+
+function proportions(){
 	ctx.fillStyle = "#FF5500";
 	var path = new Path2D;
 	path.moveTo(200,125);
@@ -73,9 +143,11 @@ function size(){
 	//head top
 	path.lineTo(300,300);
 	//hip area, where pants begin
-	path.lineTo(200,450);
+	path.lineTo(200,475);
 	//top of shoe
 
 	ctx.fill(path);
 }
+
+//proportions()
 
